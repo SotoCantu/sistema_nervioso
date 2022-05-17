f8af06a53e142fd6e774ee4f852f320432f9735b
SotoCantu <105616325+SotoCantu@users.noreply.github.com>
2022-05-17 09:59:08 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let jugarJuego = document.querySelector('.jugarJuego');
+let jugarJuegoLink = document.querySelector('.irAlJuego');
+
+jugarJuego.addEventListener('click', function () {
+    jugarJuegoLink.click();
+})
